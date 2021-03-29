3400a6cdcb13ce3e5fe9c982f65dc77ccb6d1a64
saraAhangari <sara.ahn02@gmail.com>
2021-03-29 17:41:40 +0430

    player edited

--- Player.java
+++ Player.java
@@ -6,8 +6,6 @@ public abstract class Player {
     public boolean SavedByDoctor = false;
     public boolean hasRoleOnNight;
     public int voteNum = 0;
-    protected int hearts = 0 ;
-    public Player lastVotee = null;
 
     public Player(String playerName , String playerRole) {
         this.playerName = playerName;
@@ -17,19 +15,4 @@ public abstract class Player {
         this.playerName = playerName;
     }
 
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
-
-    public String getPlayerRole() {
-        return playerRole;
-    }
-
-    public void setPlayerRole(String playerRole) {
-        this.playerRole = playerRole;
-    }
 }
