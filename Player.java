c54fdd118a899a6b6b63928fcb87b1a6c1a482da
saraAhangari <sara.ahn02@gmail.com>
2021-04-02 01:49:58 +0430

    NightRole method added

--- Player.java
+++ Player.java
@@ -1,18 +1,26 @@
 public abstract class Player {
     public String playerName;
     public String playerRole;
-    public boolean isKilled = false;
-    public boolean isSilent = false;
-    public boolean SavedByDoctor = false;
+    public String lastVote;
+    public boolean isKilled;
+    public boolean isSilent;
+    public boolean SavedByDoctor;
     public boolean hasRoleOnNight;
-    public int voteNum = 0;
+    public boolean hasExteraHeart;
+    public boolean swapped;
+    public int voteNum;
+    public int detectiveCount;
+    public int silencerCount;
+    public int votes;
 
     public Player(String playerName , String playerRole) {
         this.playerName = playerName;
         this.playerRole = playerRole;
+        this.isKilled = false;
+        this.isSilent = false;
+        this.SavedByDoctor = false;
+        this.voteNum=0;
+        this.swapped = false;
     }
-    public Player(String playerName){
-        this.playerName = playerName;
-    }
-
+    public abstract void NightRole(Player[] players , String name);
 }
