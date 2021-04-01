7073ead701fc418d2a6e2f8ccea62060ed2625d3
saraAhangari <sara.ahn02@gmail.com>
2021-04-02 01:42:13 +0430

    bulletproof player added

--- bulletproof.java
+++ bulletproof.java
@@ -3,5 +3,9 @@ public class bulletproof extends Player{
     public bulletproof(String playerName, String playerRole) {
         super(playerName, playerRole);
         super.hasRoleOnNight=false;
+        super.hasExteraHeart = true;
     }
+
+    @Override
+    public void NightRole(Player[] players , String votee){}
 }
