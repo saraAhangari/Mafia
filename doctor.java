43e883d9638890aa4aca95a96efd229280d63498
saraAhangari <sara.ahn02@gmail.com>
2021-04-02 01:41:49 +0430

    doctor player added

--- doctor.java
+++ doctor.java
@@ -4,4 +4,13 @@ public class doctor extends Player{
         super(playerName, playerRole);
         super.hasRoleOnNight=true;
     }
+
+    @Override
+    public void NightRole(Player[] players, String votee)  {
+        for (int i = 0; i < players.length ; i++) {
+            if (votee.equals(players[i].playerName)){
+                players[i].SavedByDoctor = true;
+            }
+        }
+    }
 }
